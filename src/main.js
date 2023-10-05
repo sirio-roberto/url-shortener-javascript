86f95c15f5d2ba2885a83b63e83713affe39cdf7
Sirio <sirioroberto1@hotmail.com>
2023-10-05 11:49:07 -0300

    Created URL validation

--- src/main.js
+++ src/main.js
@@ -4,5 +4,13 @@ const urlList = document.getElementById("list-url");
 
 createBtn.addEventListener("click", () => {
   const input = urlInput.value;
-  urlList.insertAdjacentHTML("beforeend", `<li><a href='${input}'>localhost/ab5dr</a> - ${input}</li>`);
+  if (isValidUrl(input)) {
+    urlList.insertAdjacentHTML("beforeend", `<li><a href='${input}'>localhost/ab5dr</a> - ${input}</li>`);
+  } else {
+    urlList.insertAdjacentHTML("beforebegin", "<p>Please enter a valid url</p>");
+  }
 });
+
+function isValidUrl(url) {
+  return /^https?:\/\/\w+\..+/i.test(url);
+}
