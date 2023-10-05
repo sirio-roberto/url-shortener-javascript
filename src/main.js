ab72ea69310df87f25db3a0ef265e336e8b082cc
Sirio <sirioroberto1@hotmail.com>
2023-10-05 12:00:24 -0300

    Implemented function to generate a short string that will be used to represent the shorter link

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const shorterUrlLength = 5;
+
 const urlInput = document.getElementById("input-url");
 const createBtn = document.getElementById("button-create");
 const urlList = document.getElementById("list-url");
@@ -5,7 +7,8 @@ const urlList = document.getElementById("list-url");
 createBtn.addEventListener("click", () => {
   const input = urlInput.value;
   if (isValidUrl(input)) {
-    urlList.insertAdjacentHTML("beforeend", `<li><a href='${input}'>localhost/ab5dr</a> - ${input}</li>`);
+    urlList.insertAdjacentHTML("beforeend",
+      `<li><a target="_blank" href='${input}'>localhost/${getRandomString(shorterUrlLength)}</a> - ${input}</li>`);
   } else {
     urlList.insertAdjacentHTML("beforebegin", "<p>Please enter a valid url</p>");
   }
@@ -13,4 +16,17 @@ createBtn.addEventListener("click", () => {
 
 function isValidUrl(url) {
   return /^https?:\/\/\w+\..+/i.test(url);
+}
+
+function getRandomInteger(limit) {
+  return Math.floor(Math.random() * limit);
+}
+
+function getRandomString(length) {
+  const possibleChars = "abcdefghijklmnopqrstuwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += possibleChars.charAt(getRandomInteger(possibleChars.length));
+  }
+  return result;
 }
