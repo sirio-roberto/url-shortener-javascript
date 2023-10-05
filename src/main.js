31a97f1b858ce06143b8a3bfc890f14d1cba5705
Sirio <sirioroberto1@hotmail.com>
2023-10-05 14:06:28 -0300

    Created function to count number of clicks that the generated URLs are receiving

--- src/main.js
+++ src/main.js
@@ -7,8 +7,25 @@ const urlList = document.getElementById("list-url");
 createBtn.addEventListener("click", () => {
   const input = urlInput.value;
   if (isValidUrl(input)) {
-    urlList.insertAdjacentHTML("beforeend",
-      `<li><a target="_blank" href='${input}'>localhost/${getRandomString(shorterUrlLength)}</a> - ${input}</li>`);
+    let clicks = 0;
+
+    const span = document.createElement("span");
+    span.innerText = `Clicks: ${clicks}`;
+
+    const link = document.createElement("a");
+    link["target"] = "_blank";
+    link["href"] = input;
+    link.innerText = `localhost/${getRandomString(shorterUrlLength)}`;
+
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${link.outerHTML} - ${input} - ${span.outerHTML}`;
+
+    listItem.addEventListener("click", (e) => {
+      span.innerText = `Clicks: ${++clicks}`
+      listItem.innerHTML = `${link.outerHTML} - ${input} - ${span.outerHTML}`;
+    });
+
+    urlList.append(listItem);
   } else {
     urlList.insertAdjacentHTML("beforebegin", "<p>Please enter a valid url</p>");
   }
